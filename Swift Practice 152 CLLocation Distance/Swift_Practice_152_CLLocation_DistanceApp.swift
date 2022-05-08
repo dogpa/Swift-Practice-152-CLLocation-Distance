4e1d1ab0550ea67e09c33c99587f076190c1a271
Dogpa <dogpa0130@gmail.com>
2022-05-08 20:58:04 +0800

    Initial Commit

---
+++ Swift Practice 152 CLLocation Distance/Swift_Practice_152_CLLocation_DistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_152_CLLocation_DistanceApp.swift
+//  Swift Practice 152 CLLocation Distance
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/8.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_152_CLLocation_DistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
